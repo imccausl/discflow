1b41379c600083d7a81ada93b3291f74a58a161f
Ian McCausland <imccausl@gmail.com>
2023-10-08 12:56:30 -0400

    wip: remove mobile header text

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -58,13 +58,6 @@ export const Header: React.FC<HeaderProps> = ({ user }) => {
                                 <HeaderLinkGroup />
                             </div>
                         </div>
-                        <div className="block md:hidden">
-                            <div className="ml-10 flex items-center space-x-4">
-                                <span className="rounded-md px-3 py-2 text-base font-medium text-white">
-                                    Log in
-                                </span>
-                            </div>
-                        </div>
                     </div>
                     <div className="hidden md:block">
                         {user ? (
