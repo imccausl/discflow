2ef0777e7f2821c36bcf560cea7a46eeb7d29884
Ian McCausland <imccausl@gmail.com>
2023-10-08 12:53:13 -0400

    wip: add global error boundary for 404

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -11,6 +11,9 @@ import {
     Scripts,
     ScrollRestoration,
     useLoaderData,
+    useRouteError,
+    Link,
+    isRouteErrorResponse,
 } from '@remix-run/react'
 
 import stylesheet from '~/tailwind.css'
@@ -44,6 +47,65 @@ export default function App() {
                 <ScrollRestoration />
                 <Scripts />
                 <LiveReload />
+            </body>
+        </html>
+    )
+}
+
+export function ErrorBoundary() {
+    const error = useRouteError()
+    console.log(error)
+    return (
+        <html lang="en" className="h-full bg-gray-100">
+            <head>
+                <meta charSet="utf-8" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <Meta />
+                <Links />
+            </head>
+            <body className="h-full">
+                <>
+                    <Header user={null} />
+                    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+                        {
+                            isRouteErrorResponse(error) &&
+                            error.status === 404 ? (
+                                <div className="text-center">
+                                    <p className="text-base font-semibold text-indigo-600">
+                                        404
+                                    </p>
+                                    <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+                                        Page not found
+                                    </h1>
+                                    <p className="mt-6 text-base leading-7 text-gray-600">
+                                        Sorry, we couldn’t find the page you’re
+                                        looking for.
+                                    </p>
+                                    <div className="mt-10 flex items-center justify-center gap-x-6">
+                                        <Link
+                                            to="/"
+                                            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                        >
+                                            Go back home
+                                        </Link>
+                                        <Link
+                                            to="/support"
+                                            className="text-sm font-semibold text-gray-900"
+                                        >
+                                            Contact support{' '}
+                                            <span aria-hidden="true">
+                                                &rarr;
+                                            </span>
+                                        </Link>
+                                    </div>
+                                </div>
+                            ) : null /* TODO: handle other errors if necessary in the global error boundary */
+                        }
+                    </main>
+                </>
             </body>
         </html>
     )
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -58,20 +58,25 @@ export const Header: React.FC<HeaderProps> = ({ user }) => {
                                 <HeaderLinkGroup />
                             </div>
                         </div>
-                    </div>
-                    <div className="hidden md:block">
-                        <div className="ml-4 flex items-center md:ml-6">
-                            <div className="relative ml-3">
-                                {user ? (
-                                    <>
-                                        <NotificationButton />
-                                        <UserMenu />
-                                    </>
-                                ) : (
-                                    <HeaderLink to="/login" label="Log in" />
-                                )}
+                        <div className="block md:hidden">
+                            <div className="ml-10 flex items-center space-x-4">
+                                <span className="rounded-md px-3 py-2 text-base font-medium text-white">
+                                    Log in
+                                </span>
                             </div>
                         </div>
+                    </div>
+                    <div className="hidden md:block">
+                        {user ? (
+                            <div className="ml-4 flex items-center md:ml-6">
+                                <NotificationButton />
+                                <div className="relative ml-3">
+                                    <UserMenu />
+                                </div>
+                            </div>
+                        ) : (
+                            <HeaderLink to="/login" label="Log in" />
+                        )}
                     </div>
 
                     <MobileMenu attachTo={mobileMenuRef}>
