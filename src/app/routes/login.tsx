99510c4262951c15e001ff54f7bd3afb6cb04896
Ian McCausland <imccausl@gmail.com>
2023-10-08 16:11:24 -0400

    wip: add registration form

--- src/app/routes/login.tsx
+++ src/app/routes/login.tsx
@@ -85,7 +85,7 @@ export default function Login() {
                 <p className="mt-10 text-center text-sm text-gray-500">
                     Not a member?{' '}
                     <Link
-                        to="/signup"
+                        to="/register"
                         className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
                     >
                         Sign up for a free account
